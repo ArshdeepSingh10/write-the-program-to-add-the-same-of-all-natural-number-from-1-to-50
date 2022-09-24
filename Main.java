056a7ee14c25955efd99ac8d42a4df2f76183c08
ArshdeepSingh10 <114170992+ArshdeepSingh10@users.noreply.github.com>
2022-09-24 19:23:44 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+	public static void main(String a[]) {
+		int sum = 0;
+		
+		for(int i = 1 ;i <= 50 ; i++) {
+			sum = sum+i;
+		}
+		System.out.println("sum of all natural number from 1 to 50 = "+sum);
+		
+		
+	}
+}
